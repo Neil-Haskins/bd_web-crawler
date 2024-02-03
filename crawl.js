f5b7b2d83b2872db4362788afa075f911ea90dbc
Neil Haskins <neil.haskins@studyforge.net>
2024-02-02 21:41:18 -0800

    Add getURLsFromHTML and test. Update node version.

--- crawl.js
+++ crawl.js
@@ -1,11 +1,27 @@
+const { JSDOM } = require('jsdom');
+
 function normalizeURL(urlString) {
     if (urlString.slice(0,2) === '//') {
         urlString = 'http:' + urlString;
     }
-    const url = new URL(urlString)
+    const url = new URL(urlString);
     return url.hostname + url.pathname
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+    const { document } = (new JSDOM(htmlBody)).window;
+    const aEls = [...document.querySelectorAll('a[href]')];
+    return aEls.map((a) => {
+        if ( URL.canParse(a.href) ) {
+            return a.href
+        } else {
+            const url = new URL(a.href, baseURL)
+            return url.href
+        }
+    });
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@jest/globals');
-const { normalizeURL } = require('./crawl.js');
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js');
 
 test('Normalize https://example.com/', () => {
     expect(normalizeURL('https://example.com/'))
@@ -24,4 +24,26 @@ test('Normalize //example.com/index.html', () => {
 test('Normalize http://example.com/dir', () => {
     expect(normalizeURL('http://example.com/dir'))
     .toBe('example.com/dir');
+});
+
+
+
+test('Get links from htmlBody', () => {
+    const htmlBody = `<body>
+<div>
+    <a href="https://www.gnu.org/software/emacs/">Yay</a>
+</div>
+<p><a href="https://en.wikipedia.org/wiki/Bear" target="_blank">BEARS!</a></p>
+<a href="page.html">Same folder</a>
+<a href="/at-root.html">root level</a>
+<a href="../up/here.html">up a level in a folder</a>
+</body>`;
+    expect(getURLsFromHTML(htmlBody, 'https://awesomesite.com/dir1/dir2/my-page.html'))
+    .toStrictEqual([
+        'https://www.gnu.org/software/emacs/',
+        'https://en.wikipedia.org/wiki/Bear',
+        'https://awesomesite.com/dir1/dir2/page.html',
+        'https://awesomesite.com/at-root.html',
+        'https://awesomesite.com/dir1/up/here.html'
+    ]);
 });
